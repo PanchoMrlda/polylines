59a6b0d3d376bbf632a0f4e5b80322c82738f0ca
PanchoMrlda <fco.moraledac@gmail.com>
2019-06-19 10:23:34 +0200

    Creating helper class to access aws data easier

---
+++ helpers/aws/DynamoDbHelper.php
@@ -0,0 +1,62 @@
+<?php
+
+class DynamoDbHelper
+{
+  function __construct(Aws\DynamoDb\DynamoDbClient $dynamodb, Aws\DynamoDb\Marshaler $marshaler)
+  {
+    $this->dynamodb = $dynamodb;
+    $this->marshaler = $marshaler;
+  }
+
+  public function getDataFromDynamo(String $deviceId, Int $from = null, Int $to = null)
+  {
+    $params = $this->initParams($deviceId, $from, $to);
+    $raw_data = $this->dynamodb->query($params);
+    if (count($raw_data['Items']) != 0) {
+      $formattedData = $this->getFormattedResult($raw_data);
+      $payloads = call_user_func_array('array_merge', array_column($formattedData, 'payload'));
+    } else {
+      $payloads = [];
+    }
+    return $payloads;
+  }
+
+  private function initParams(String $deviceId, Int $from = null, Int $to = null)
+  {
+    $from_timestamp = $from || (time() - 60 * 5) * 1000;
+    $to_timestamp = $to || (time() - 60 * 0) * 1000;
+
+    $eav = $this->marshaler->marshalJson('
+      {
+        ":deviceToFind": "' . $deviceId . '",
+        ":fromTimeStamp": ' . $from . ',
+        ":toTimeStamp": ' . $to . '
+      }
+    ');
+
+    $params = [
+      'TableName' => 'DevicesDataTable',
+      'KeyConditionExpression' => "deviceId = :deviceToFind AND receivedTimeStamp BETWEEN :fromTimeStamp AND :toTimeStamp",
+      'ExpressionAttributeValues' => $eav,
+      'ConsistentRead' => false,
+      'ScanIndexForward' => false
+    ];
+
+    return $params;
+  }
+
+  private function getFormattedResult(Aws\Result $dynamoJson)
+  {
+    $formattedResult = [[]];
+    $locations = [];
+    foreach ($dynamoJson['Items'] as $message) {
+      $data = array(
+        'deviceId' => $this->marshaler->unmarshalValue($message['deviceId']),
+        'receivedTimeStamp' => $this->marshaler->unmarshalValue($message['receivedTimeStamp']),
+        'payload' => $this->marshaler->unmarshalValue($message['payload'])
+      );
+      array_unshift($formattedResult, $data);
+    }
+    return array_filter($formattedResult);
+  }
+}
