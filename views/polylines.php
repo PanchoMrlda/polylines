dfa1e53edc0a268fa800e85a4312f96976683235
PanchoMrlda <fco.moraledac@gmail.com>
2019-08-08 10:15:48 +0200

    Move styles to external file

--- views/polylines.php
+++ views/polylines.php
@@ -9,52 +9,13 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <script src="https://d3js.org/d3.v5.min.js"></script>
   <link href="css/c3.css" rel="stylesheet">
+  <link href="css/polylines.css" rel="stylesheet">
   <script src="js/c3.min.js"></script>
   <title>Simple Polylines</title>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <style>
-    /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-    #map {
-      height: 100%;
-    }
-
-    /* Optional: Makes the sample page fill the window. */
-    html,
-    body {
-      height: 100%;
-      margin: 0;
-      padding: 0;
-      width: 100%;
-    }
-
-    .span {
-      width: 180px;
-      min-width: 180px;
-      margin: 0 10px;
-      display: inline-block;
-      text-align: center;
-    }
-
-    .input {
-      width: 180px;
-      min-width: 180px;
-      margin: 0 10px;
-      text-align: center;
-    }
-
-    .select {
-      text-align: center;
-      text-align-last: center;
-    }
-
-    .select option {
-      text-align: left;
-    }
-  </style>
 </head>
 
 <body onload="initMap()">
@@ -103,10 +64,10 @@
       </section>
     </form>
   </section>
-  <div id="map"></div>
-  <div id="tempChart"></div>
-  <div id="pressureChart"></div>
-  <div id="voltageChart"></div>
+  <div id="map" class="chart"></div>
+  <div id="tempChart" class="chart"></div>
+  <div id="pressureChart" class="chart"></div>
+  <div id="voltageChart" class="chart"></div>
   <script>
     var dates1 = <?php echo (json_encode($dates1)) ?>;
     dates1.unshift('times');
