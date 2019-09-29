41512e7c09766b9d7fc4f39fd57f50fecc9f2ac4
PanchoMrlda <fco.moraledac@gmail.com>
2019-09-29 21:54:31 +0200

    Fixing wrong syntax in polylines

--- views/polylines.php
+++ views/polylines.php
@@ -2,21 +2,21 @@
 <html>
 
 <head>
-  <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+  <meta charset="utf-8" />
   <script type="text/javascript" src="js/mapStyles.js"></script>
-  <script async type="text/javascript" src="js/mapLogic.js"></script>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <script async="async" type="text/javascript" src="js/mapLogic.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   <script src="js/d3.v5.min.js"></script>
-  <link href="css/c3.css" rel="stylesheet">
-  <link href="css/polylines.css" rel="stylesheet">
+  <link href="css/c3.css" rel="stylesheet" />
+  <link href="css/polylines.css" rel="stylesheet" />
   <script src="js/c3.min.js"></script>
   <title>Simple Polylines</title>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="icon" type="image/x-icon" href="favicon.ico" />
 </head>
 
 <body onload="initMap()">
@@ -24,16 +24,16 @@
     <form action="/">
       <section class="form-last-reading-section input-group-prepend">
         <span class="justify-content-center span input-group-text">Previous reading</span>
-        <input class="input" type="text" value="<?php echo $lastReading1; ?>" name="from" onchange="this.form.submit()" disabled>
-        <input class="input" type="text" value="<?php echo $lastReading2; ?>" name="from" onchange="this.form.submit()" disabled>
+        <input class="input" type="text" value="<?php echo $lastReading1; ?>" name="from" disabled="disabled" />
+        <input class="input" type="text" value="<?php echo $lastReading2; ?>" name="from" disabled="disabled" />
       </section>
       <section class="form-date-section input-group-prepend">
         <span class="justify-content-center span input-group-text">Date</span>
-        <input class="input" type="date" value="<?php echo date('Y-m-d', $from / 1000); ?>" name="from" onchange="this.form.submit()">
+        <input class="input" type="date" value="<?php echo date('Y-m-d', $from / 1000); ?>" name="from" onchange="submitForm()" />
       </section>
       <section class="form-device-section input-group-prepend">
         <span class="justify-content-center span input-group-text">Device Name</span>
-        <select class="select input" name="deviceId1" id="deviceId1Select" onchange="this.form.submit()">
+        <select class="select input" name="deviceId1" id="deviceId1Select" onchange="submitForm()">
           <option value="">-</option>
           <?php
           foreach ($deviceNames as $deviceZone => $deviceList) {
@@ -45,7 +45,7 @@
           }
           ?>
         </select>
-        <select class="select input" name="deviceId2" id="deviceId2Select" onchange="this.form.submit()">
+        <select class="select input" name="deviceId2" id="deviceId2Select" onchange="submitForm()">
           <option value="">-</option>
           <?php
           foreach ($deviceNames as $deviceZone => $deviceList) {
@@ -60,8 +60,8 @@
       </section>
       <section class="form-distance-section input-group-prepend">
         <span class="justify-content-center span input-group-text">Travelled distance</span>
-        <input class="input" type="text" value="" name="distance1" id="distance1" disabled>
-        <input class="input" type="text" value="" name="distance2" id="distance2" disabled>
+        <input class="input" type="text" value="" name="distance1" id="distance1" disabled="disabled" />
+        <input class="input" type="text" value="" name="distance2" id="distance2" disabled="disabled" />
       </section>
     </form>
   </section>
@@ -97,7 +97,7 @@
     var tempInt2 = <?php echo (json_encode($tempInt2)) ?>;
     tempInt2.unshift('Temp Int' + ' (<?php echo $deviceId2 ?>)');
     var tempExt2 = <?php echo (json_encode($tempExt2)) ?>;
-    tempExt2.unshift('Temp Ext'  + ' (<?php echo $deviceId2 ?>)');
+    tempExt2.unshift('Temp Ext' + ' (<?php echo $deviceId2 ?>)');
     var highPressure2 = <?php echo (json_encode($highPressure2)) ?>;
     highPressure2.unshift('High Pressure' + ' (<?php echo $deviceId2 ?>)');
     var lowPressure2 = <?php echo (json_encode($lowPressure2)) ?>;
@@ -107,9 +107,7 @@
     var blower2 = <?php echo (json_encode($blower2)) ?>;
     blower2.unshift('Blower' + ' (<?php echo $deviceId2 ?>)');
   </script>
-  <script async defer src="https://maps.googleapis.com/maps/api/js?key=
-    <?php echo $secretsData['google']['mapsKey'] ?>
-    &libraries=visualization">
+  <script async="async" defer="defer" src="https://maps.googleapis.com/maps/api/js?key=<?php echo $_SESSION['secretsData']['google']['mapsKey'] ?>">
   </script>
 </body>
 
