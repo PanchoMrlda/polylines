68cc436c16a89e8f0a7879fb2da0883a3e00cd72
PanchoMrlda <fco.moraledac@gmail.com>
2019-11-18 08:55:46 +0100

    Remove nav and spinner from views and create layouts for them

--- views/polylines.php
+++ views/polylines.php
@@ -5,23 +5,25 @@
   <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
   <meta charset="utf-8" />
   <script type="text/javascript" src="js/mapStyles.js"></script>
+  <script type="text/javascript" src="js/utils.js"></script>
   <script async="async" type="text/javascript" src="js/mapLogic.js"></script>
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   <script src="js/d3.v5.min.js"></script>
   <link href="css/c3.css" rel="stylesheet" />
   <link href="css/polylines.css" rel="stylesheet" />
   <script src="js/c3.min.js"></script>
-  <title>Simple Polylines</title>
+  <title>Simple Polylines - Map</title>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <link rel="icon" type="image/x-icon" href="favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="/favicon.ico" />
 </head>
 
 <body onload="initMap()">
+  <?php include 'views/layouts/nav.php'; ?>
   <section>
-    <form action="/">
+    <form class="map-form" action="/">
       <section class="form-last-reading-section input-group-prepend">
         <span class="justify-content-center span input-group-text">Previous reading</span>
         <input class="input" type="text" value="<?php echo $lastReading1; ?>" name="from1" disabled="disabled" />
@@ -131,8 +133,6 @@
   </script>
 </body>
 
-<div class="spinner-border text-primary" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
+<?php include 'views/layouts/spinner.php'; ?>
 
 </html>
