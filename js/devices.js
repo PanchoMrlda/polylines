12df71e3e6d4359854b0ca46e3c3b946ac472e51
PanchoMrlda <fco.moraledac@gmail.com>
2019-11-20 10:00:50 +0100

    Adding changes to access table instead of raw inputs in devices.js

--- js/devices.js
+++ js/devices.js
@@ -1,6 +1,6 @@
 function setConfig() {
   var requestParams = {};
-  var inputs = document.querySelectorAll(".input-checkbox-large input:last-child");
+  var inputs = document.querySelectorAll("[type=hidden]");
   inputs.forEach(input => {
     var section = {};
     Array.prototype.slice.call(input.attributes).forEach(attribute => {
@@ -9,12 +9,23 @@ function setConfig() {
       }
     });
     requestParams[input.name] = section;
+    if (input.getAttribute("sectiontype") == "controller node") {
+      var globalParams = {
+        sectiontype: "global params",
+        name: "Params_nodo" + input.getAttribute("nodeid"),
+        nodeid: input.getAttribute("nodeid"),
+        pwmvoltage: 5,
+        pwmfrequencyhz: 250
+      };
+      requestParams[globalParams.name] = globalParams;
+    }
   });
   doRequest("POST", "/config", undefined, requestParams, "application/json");
+  window.open("/config.hvc", "_blank");
 }
 
 function updateSection(element) {
-  var targetElement = element.parentElement.lastElementChild;
+  var targetElement = element.parentElement.parentElement.lastElementChild.lastElementChild;
   var customAttr = document.createAttribute(element.name);
   customAttr.value = element.value;
   targetElement.attributes.setNamedItem(customAttr);
@@ -22,7 +33,7 @@ function updateSection(element) {
 
 function resetForm(element) {
   element.form.reset();
-  var inputs = document.querySelectorAll(".input-checkbox-large input:last-child");
+  var inputs = document.querySelectorAll("[type=hidden]");
   inputs.forEach(input => {
     Array.prototype.slice.call(input.attributes).forEach(attribute => {
       if (attribute.name != "class" && attribute.name != "type") {
@@ -31,3 +42,22 @@ function resetForm(element) {
     });
   });
 }
+
+/**
+ * EVENTS
+ */
+var headers = document.querySelectorAll("body > section > table > thead > tr > th");
+Array.prototype.map.call(headers, function (header) {
+  header.addEventListener("click", function () {
+    var selector = header.getAttribute("section");
+    var targetElements = document.querySelectorAll("." + selector + "");
+    Array.prototype.map.call(targetElements, function (targetElement) {
+      var visibility = targetElement.style.visibility;
+      if (visibility == "collapse" || visibility == "") {
+        targetElement.style.visibility = "visible";
+      } else {
+        targetElement.style.visibility = "collapse";
+      }
+    });
+  });
+});
