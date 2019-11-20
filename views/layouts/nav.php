10e811d73c5544d6427e72fbf991cae2a0d12fa5
PanchoMrlda <fco.moraledac@gmail.com>
2019-11-20 09:57:54 +0100

    Don't add static active class to nav layout

--- views/layouts/nav.php
+++ views/layouts/nav.php
@@ -1,6 +1,6 @@
-<nav class="nav nav-pills nav-fill nav-justified d-flex justify-content-around flex-nowrap">
+<nav class="nav nav-pills nav-fill nav-justified d-flex justify-content-around flex-nowrap blue-gradient lighten-3 py-2 font-weight-bold">
   <a class="text-sm-center nav-link" href="/">Map</a>
-  <a class="text-sm-center nav-link active" href="/devices/config">Devices Config</a>
+  <a class="text-sm-center nav-link" href="/devices/config">Devices Config</a>
   <a class="text-sm-center nav-link" href="/handwriting">Handwriting</a>
   <a class="text-sm-center nav-link disabled" href="#">Disabled</a>
 </nav>
