3d716ff196625ebb7d9c02b4f7777365fcc48d17
PanchoMrlda <fco.moraledac@gmail.com>
2020-04-23 13:06:54 +0200

    Updating views and bootstrap libraries for responsiveness in polylines.php

--- views/polylines.php
+++ views/polylines.php
@@ -36,71 +36,73 @@ function printDevices($deviceData)
     <link href="css/polylines.css" rel="stylesheet"/>
     <script src="js/c3.min.js"></script>
     <title>Simple Polylines - Map</title>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
             crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
             crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
             crossorigin="anonymous"></script>
     <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
 </head>
 
 <body onload="initMap()">
 <?php include 'views/layouts/nav.php'; ?>
-<section class="form-container">
-    <form class="map-form" action="/">
-        <section class="form-last-reading-section input-group-prepend">
-            <span class="justify-content-center span input-group-text">Previous reading</span>
-            <input class="input" type="text" value="<?php echo $lastReading1; ?>" name="from1" disabled="disabled"/>
-            <input class="input" type="text" value="<?php echo $lastReading2; ?>" name="from2" disabled="disabled"/>
-        </section>
-        <section class="form-date-section input-group-prepend">
-            <span class="justify-content-center span input-group-text">Date</span>
-            <input class="input" type="date" value="<?php echo date('Y-m-d', $from / 1000); ?>" name="from"
-                   onchange="submitForm()"/>
-            <span class="justify-content-center input-checkbox">
+<section class="options-container">
+    <section class="p-2 flex-fill form-container">
+        <form class="map-form" action="/">
+            <section class="form-last-reading-section input-group-prepend">
+                <span class="justify-content-center span input-group-text">Previous reading</span>
+                <input class="input" type="text" value="<?php echo $lastReading1; ?>" name="from1" disabled="disabled"/>
+                <input class="input" type="text" value="<?php echo $lastReading2; ?>" name="from2" disabled="disabled"/>
+            </section>
+            <section class="form-date-section input-group-prepend">
+                <span class="justify-content-center span input-group-text">Date</span>
+                <input class="input" type="date" value="<?php echo date('Y-m-d', $from / 1000); ?>" name="from"
+                       onchange="submitForm()"/>
+                <span class="justify-content-center input-checkbox">
           <input class=" input checkbox" type="checkbox" name="lastHour" onchange="submitForm()"/>
           Last Hour
         </span>
-        </section>
-        <section class="form-date-section input-group-prepend">
-            <span class="justify-content-center span input-group-text">Data</span>
-            <span class="justify-content-center input-checkbox">
+            </section>
+            <section class="form-date-section input-group-prepend">
+                <span class="justify-content-center span input-group-text">Data</span>
+                <span class="justify-content-center input-checkbox">
           <input class=" input checkbox" type="checkbox" name="pressureInBars" onchange="submitForm()"/>
           Pressure in bars
         </span>
-        </section>
-        <section class="form-device-section input-group-prepend">
-            <span class="justify-content-center span input-group-text">Device Name</span>
-            <input class="select input" list="devicesList1" id="deviceId1Select" name="deviceId1"
-                   onchange="submitForm()">
-            <datalist id="devicesList1">
-                <?php
-                printDevices($deviceData);
-                ?>
-            </datalist>
-            <input class="select input" list="devicesList2" id="deviceId2Select" name="deviceId2"
-                   onchange="submitForm()">
-            <datalist id="devicesList2">
-                <?php
-                printDevices($deviceData);
-                ?>
-            </datalist>
-        </section>
-        <section class="form-distance-section input-group-prepend">
-            <span class="justify-content-center span input-group-text">Travelled distance</span>
-            <input class="input" type="text" value="" name="distance1" id="distance1" disabled="disabled"/>
-            <input class="input" type="text" value="" name="distance2" id="distance2" disabled="disabled"/>
-        </section>
-    </form>
-</section>
-<section class="map-container">
-    <div id="map" class="chart"></div>
+            </section>
+            <section class="form-device-section input-group-prepend">
+                <span class="justify-content-center span input-group-text">Device Name</span>
+                <input class="select input" list="devicesList1" id="deviceId1Select" name="deviceId1"
+                       onchange="submitForm()">
+                <datalist id="devicesList1">
+                    <?php
+                    printDevices($deviceData);
+                    ?>
+                </datalist>
+                <input class="select input" list="devicesList2" id="deviceId2Select" name="deviceId2"
+                       onchange="submitForm()">
+                <datalist id="devicesList2">
+                    <?php
+                    printDevices($deviceData);
+                    ?>
+                </datalist>
+            </section>
+            <section class="form-distance-section input-group-prepend">
+                <label class="justify-content-center span input-group-text" for="distance1">Travelled distance</label>
+                <input class="input" type="text" value="" name="distance1" id="distance1" disabled="disabled"/>
+                <input class="input" type="text" value="" name="distance2" id="distance2" disabled="disabled"/>
+            </section>
+        </form>
+    </section>
+    <section class="p-2 flex-fill map-container">
+        <div id="map" class="chart"></div>
+    </section>
 </section>
 <div id="tempChart" class="chart"></div>
 <div id="pressureChart" class="chart"></div>
