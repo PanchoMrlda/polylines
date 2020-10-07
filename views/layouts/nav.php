abafd504fa98d2f5f3664b789077f79b056d811a
PanchoMrlda <fco.moraledac@gmail.com>
2020-10-07 15:31:33 +0200

    Changing website title in nav.php

--- views/layouts/nav.php
+++ views/layouts/nav.php
@@ -13,7 +13,7 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
             <span class="navbar-toggler-icon"></span>
         </button>
-            <a class="navbar-brand mx-auto" href="/">Noshanrobaoelbule</a>
+            <a class="navbar-brand mx-auto" href="/">Telematic</a>
     </div>
     <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
         <ul class="navbar-nav ml-auto">
