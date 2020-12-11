a60d38d11a7ac1dcfe8766bd2d96fbddfc9a8dff
PanchoMrlda <fco.moraledac@gmail.com>
2020-12-11 22:24:33 +0100

    Defining all asset files in webpack.mix.js

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+mix.setPublicPath(path.normalize('public_html'));
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +12,22 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.styles([
+    'resources/css/app.css',
+    'resources/css/c3.css',
+], 'public_html/css/front.css')
+    .styles([
+    'resources/css/handwriting.css',
+], 'public_html/css/handwriting.css');
+mix.scripts([
+    'resources/js/utils.js',
+], 'public_html/js/front.js')
+    .scripts([
+        'resources/js/c3.min.js',
+        'resources/js/d3.v5.min.js',
+        'resources/js/mapStyles.js',
+        'resources/js/mapLogic.js',
+    ], 'public_html/js/map.js')
+    .scripts([
+        'resources/js/handwriting.js',
+    ], 'public_html/js/handwriting.js');
