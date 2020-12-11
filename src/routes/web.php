196dcba266ce7a6f469ec40ef6334e7894ffeac8
PanchoMrlda <fco.moraledac@gmail.com>
2020-12-11 22:17:09 +0100

    Adding main app routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/devices/config', 'HomeController@index')->name('devices/config');
+Route::get('/handwriting', 'HomeController@handwriting')->name('handwriting');
+Route::get('/profile', 'HomeController@profile')->name('profile');
+Route::get('/dynamo', 'DynamoDbController@show')->name('dynamo');
+Route::post('/handwriting/image', 'HandwritingController@image')->name('handwriting/image');
