fc016b99a0afa1d0e6b4413208878a06bb79ee0a
PanchoMrlda <fco.moraledac@gmail.com>
2020-12-11 22:21:11 +0100

    Adding main app translations

---
+++ src/resources/lang/en/messages.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'general' => [
+        'minute' => 'Minute',
+        'minutes' => 'Minutes',
+        'day' => 'Day',
+        'days' => 'Days',
+        'year' => 'Year',
+        'years' => 'Years',
+        'loading' => 'Loading',
+    ],
+    'layout_names' => [
+        'dashboard' => 'Dashboard',
+        'config_file' => 'Config File',
+        'handwriting' => 'Handwriting',
+    ],
+    'polylines_layout' => [
+        'previous_reading' => 'Previous reading',
+        'date' => 'Date',
+        'hours' => 'Hours',
+        'last_hour' => 'Last Hour',
+        'data' => 'Data',
+        'pressure_in_bars' => 'Pressure in bars',
+        'device_name' => 'Device Name',
+        'travelled_distance' => 'Travelled distance',
+    ]
+];
