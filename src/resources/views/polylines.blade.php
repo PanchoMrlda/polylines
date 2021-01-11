9bd38cc873b943d7aa9aefe6cba362e113564446
PanchoMrlda <fco.moraledac@gmail.com>
2021-01-11 12:51:47 +0100

    Updating the title of the views

--- src/resources/views/polylines.blade.php
+++ src/resources/views/polylines.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no"/>
     <meta charset="utf-8"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-    <title>{{ config('app.name') }}</title>
+    <title>{{ __('messages.layout_names.dashboard') }} | {{ config('app.name') }}</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
@@ -48,7 +48,6 @@
                     {{ __('messages.polylines_layout.hours') }}
                 </span>
             </section>
-
             <section class="form-date-section input-group-prepend">
                 <span class="justify-content-center span input-group-text">{{ __('messages.polylines_layout.data') }}</span>
                 <span class="justify-content-center input-checkbox d-none">
--- src/resources/views/raspberryPi.blade.php
+++ src/resources/views/raspberryPi.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no"/>
     <meta charset="utf-8"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-    <title>{{ config('app.name') }}</title>
+    <title>{{ __('messages.layout_names.raspberry_pi') }} | {{ config('app.name') }}</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
